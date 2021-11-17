621f9efd0af1d4d1c292b7113848d96dda948f54
solomon689 <bastianyima@gmail.com>
2021-11-17 12:23:28 -0300

    Carpeta Android agregada

---
+++ app-frontend/android/app/src/main/java/io/ionic/starter/MainActivity.java
@@ -0,0 +1,15 @@
+package io.ionic.starter;
+
+import android.os.Bundle;
+
+import com.getcapacitor.BridgeActivity;
+import com.codetrixstudio.capacitor.GoogleAuth.GoogleAuth;
+
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        registerPlugin(GoogleAuth.class);
+    }
+}
